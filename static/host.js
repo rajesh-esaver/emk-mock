fdf202274cb8e7623adddf0d8263525e1551ae61
Rajesh <rajesh@esavertec.com>
2021-11-14 22:28:12 +0530

    added sample controlls for host

--- static/host.js
+++ static/host.js
@@ -13,6 +13,9 @@ var interval = 1000;
 var isPaused = false;
 var secondsEle = "";
 var timer = "";
+var currQuestion = "";
+var isLifeLinesBeingShowed = true;
+var lifeLines = ["Line 1", "Line 2", "Line 3"];
 
 // questions
 const options = ["Option A", "Option B", "Option C", "Option D"];
@@ -53,17 +56,90 @@ function addEventListeners() {
     // buttons
     var pauseButton = document.getElementById("pause");
     var resumeButton = document.getElementById("resume");
+    var lifeLinesButton = document.getElementById("btn_lifelines");
+    var answerButton = document.getElementById("btn_answer");
 
     pauseButton.addEventListener("click", (e) => {
         e.preventDefault();
-        isPaused = true;
+        //isPaused = true;
+        pauseTimer()
     });
 
     resumeButton.addEventListener("click", (e) => {
         e.preventDefault();
-        isPaused = false;
+        //isPaused = false;
+        resumeTimer();
     });
 
+    answerButton.addEventListener("click", (e) => {
+        console.log("answer "+currQuestion.options[0]);
+    });
+
+    lifeLinesButton.addEventListener("click", (e) => {
+        var divLifeLines = document.getElementById("div_lifelines");
+        console.log(isLifeLinesBeingShowed);
+        if(isLifeLinesBeingShowed) {
+            $(".div_lifelines").hide();
+        } else {
+            $(".div_lifelines").show();
+        }
+        isLifeLinesBeingShowed = !isLifeLinesBeingShowed;
+    });
+
+}
+
+function pauseTimer() {
+    isPaused = true;
+}
+
+function resumeTimer() {
+    isPaused = false;
+}
+
+function clearOptions() {
+
+}
+
+function optionListener(button, option) {
+    button.onclick = function() {
+        pauseTimer();
+        button.style.background='#ffff';
+        socket.emit("set_locked_answer", option);
+    };
+}
+
+function questionListener(button, question) {
+    button.onclick = function() {
+        showQuestion(question);
+        button.style.background='#ffff';
+        if(isSocketConnected) {
+            socket.send(question);
+            startTimer();
+        }
+    };
+}
+
+function showQuestion(question) {
+    currQuestion = question;
+    const txt_question = document.getElementById("txt_question");
+    txt_question.innerHTML = question.question;
+
+    // create dynamic option buttons
+    const option_a = document.getElementById("option_a");
+    const option_b = document.getElementById("option_b");
+    const option_c = document.getElementById("option_c");
+    const option_d = document.getElementById("option_d");
+
+    console.log(question.options);
+    option_a.innerHTML = question.options[0];
+    option_b.innerHTML = question.options[1];
+    option_c.innerHTML = question.options[2];
+    option_d.innerHTML = question.options[3];
+
+    optionListener(option_a, 0);
+    optionListener(option_b, 0);
+    optionListener(option_c, 0);
+    optionListener(option_d, 0);
 }
 
 function loadQuestions() {
@@ -79,13 +155,7 @@ function loadQuestions() {
         element.innerHTML = "Question "+(i+1);
         element.className = 'btn-styled';
 
-        element.onclick = function() { // Note this is a function
-            console.log(question.question);
-            if(isSocketConnected) {
-                socket.send(question);
-                startTimer();
-            }
-        };
+        questionListener(element, question);
 
         questionsDev.appendChild(element);
     }
